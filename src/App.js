f877ee3dabdbd41b673b44d836532f5d137380e5
Vishal-Bade <vishalbade1997@gmail.com>
2023-04-17 08:36:21 +0530

    todo list

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
+
+const App = () => {
+  const [item, setItem] = useState();
+  const [newItem, setNewItem] = useState([]);
+
+  const itemEvent = (event) => {
+    setItem(event.target.value);
+  };
+
+  const AddItems = () => {
+    setNewItem((oldItems) => {
+      return [...oldItems, item];
+    });
+    setItem("");
+  };
+
+  const deleteItem = (id) => {
+    // console.log('Deleted');
+    setNewItem((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      })
+    })
+  };
+
+
+  return (
+    <div className="main_div">
+      <div className="center_div">
+        <br />
+        <h1> ToDo List </h1>
+        <br />
+        <input
+          type="text"
+          value={item}
+          placeholder="  Add An Items  "
+          onChange={itemEvent}
+        />
+        <button className="btn" onClick={AddItems}> + </button>
+
+        <ul type="none" className="ul_div">
+          {newItem.map((itemval, index) => {
+            return <ToDoList key={index} id={index} text={itemval} onSelect={deleteItem} />;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
